da8e4b10f170ebeb210fbc442a20f31976327ffa
solyps <giuseppe.cappiello23@outlook.com>
2025-05-04 13:44:11 +0200

    Adds custom 404 error page and improves request handling
    Implements a dedicated 404 error page for better user experience.
    
    Refactors request handling to differentiate between valid and invalid endpoints, including a sleep feature for the '/sleep' route.
    
    Enhances code readability by simplifying request processing logic.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fmt::format, fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
+use std::{fmt::format, fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}, thread, time::Duration};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -12,20 +12,32 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // println!("Request: {http_request:#?}");
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
     let length = contents.len();
 
     let response = 
-        format!("{status_line}\r\nContent-legth: {length}\r\n\r\n{contents}");
+    format!("{status_line}\r\nContent-legth: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+
+    // println!("Request: {http_request:#?}");
+    
 
 }
